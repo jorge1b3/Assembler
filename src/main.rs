e1855dae0b69927794fad99a9346816b4c6309d2
jorge <locodeira1b3@gmail.com>
2023-10-12 18:53:28 -0500

    Conviertiendo el código en un estilo más funcional, y haciendo que el clasificador funcione correctamente

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,14 @@ use std::env;
 use std::fs::File;
 use std::io::{self,BufRead,BufReader};
 
+enum Instruction {
+    AInstruction(String), // -> Addressing instruction
+    CInstruction(String, String, String), // -> Computation instruction
+    LInstruction(String), // -> Label instruction
+}
+
 fn main() -> io::Result<()> {
+
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
@@ -11,27 +18,92 @@ fn main() -> io::Result<()> {
     }
 
     let file_name = &args[1];
-    
-    let lines = parser(file_name)?;
-    println!("{:?}", lines);
-    Ok(())
-}
-
-// The parser read the file and return a vector of lines
-fn parser(file_name: &str) -> io::Result<Vec<String>> {
     let file = File::open(file_name)?;
     let reader = BufReader::new(file);
 
-    let mut lines: Vec<String> = Vec::new();
+    let lines = reader
+        .lines()
+        .map(|line| line.unwrap()
+            .trim().to_string())
+        .filter(|line| !line.is_empty() && !line.starts_with("//"));
 
-    for line in reader.lines() {
-        let line = line?;
-        let line = line.trim();
+    let instructions = lines.map(|line| parser(line));
 
-        if !line.is_empty() && !line.starts_with("//") {
-            lines.push(line.to_string());
+    for instruction in instructions {
+        match instruction {
+            Instruction::AInstruction(a_instruction) => println!("A instruction: {}", a_instruction),
+            Instruction::CInstruction(dest, comp, jump) => println!("C instruction: {} {} {}", dest, comp, jump),
+            Instruction::LInstruction(l_instruction) => println!("L instruction: {}", l_instruction),
         }
     }
 
-    Ok(lines)
+    // for line in reader.lines() {
+    //     let line = line?;
+    //     let line = line.trim();
+
+    //     if !line.is_empty() && !line.starts_with("//") {
+    //         println!("{}", line);
+    //     }
+    // }
+    
+    // // let lines = reader_and_cleaner(file_name)?;
+    // // let instructions: Vec<Instruction> = parser(lines);
+
+    // //let dest = [("null", [0,0,0]), ("M", [0,0,1]), ("D", )];
+    // for instruction in instructions {
+    //     match instruction {
+    //         Instruction::AInstruction(a_instruction) => println!("A instruction: {}", a_instruction),
+    //         Instruction::CInstruction(dest, comp, jump) => println!("C instruction: {} {} {}", dest, comp, jump),
+    //         Instruction::LInstruction(l_instruction) => println!("L instruction: {}", l_instruction),
+    //     }
+    // }
+    Ok(())
+}
+
+// fn reader_and_cleaner(file_name: &str) -> io::Result<Vec<String>> {
+//     let file = File::open(file_name)?;
+//     let reader = BufReader::new(file);
+
+//     let mut lines: Vec<String> = Vec::new();
+
+//     for line in reader.lines() {
+//         let line = line?;
+//         let line = line.trim();
+
+//         if !line.is_empty() && !line.starts_with("//") {
+//             line.to_string()
+//         }
+//     }
+
+//     Ok(lines)
+// }
+
+fn parser(line: String) -> Instruction {
+    if line.starts_with("@") {
+        Instruction::AInstruction(String::from(line))
+    } else if line.starts_with("(") && line.ends_with(")") {
+        Instruction::LInstruction(String::from(line))
+    } else {
+        let mut copy: &str = & line.clone();
+        let mut jump = "";
+        let dest = match copy.contains("=") {
+            true => {
+                let mut parts = copy.split("=");
+                let temp = parts.next().unwrap();
+                copy = parts.next().unwrap();
+                temp
+            },
+            false => "",
+        };
+        let comp = match copy.contains(";") {
+            true => {
+                let mut parts = copy.split(";");
+                copy = parts.next().unwrap();
+                jump = parts.next().unwrap();
+                copy
+            },
+            false => copy
+        };
+        Instruction::CInstruction(String::from(dest), String::from(comp), String::from(jump))
+    }
 }
